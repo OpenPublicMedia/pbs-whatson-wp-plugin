92cdf988ef5a8aad815ab8fd37ae446cd97878e8
Thomas Crenshaw <twcrenshaw@pbs.org>
2013-06-05 10:04:22 -0700

    updated widget template to override before and after widget

--- lib/pbs-whatson-widget-tpl.php
+++ lib/pbs-whatson-widget-tpl.php
@@ -8,6 +8,13 @@ function whatson_displayProgramAirdates($programAirdates, $programTitle, $instan
 	}
 
 	$title = apply_filters('widget_title', $instance['title'] );  
+
+	/* 6/3/13 Phil - Temporarily set $before_widget, $after_widget, $before_title, $after_title here until we figure out why they're not working on their own. */
+	$before_widget = '<div id="pbs-whats-on" class="widget widget_listings"><div class="widget-wrap">';
+	$after_widget = '</div></div>';
+	$before_title = '<h3 class="widgettitle"><span>';
+	$after_title = '</span></h3>';
+
 	/* Before widget (defined by themes). */
 	echo $before_widget;
 	/* Display the widget title if one was input (before and after defined by themes). */
@@ -33,7 +40,7 @@ function whatson_displayProgramAirdates($programAirdates, $programTitle, $instan
 					$episode_title = "TBD";
 				}
 ?>
-			<ul id="whatson">
+			<ul class="listing">
 				<li class="station"><strong>Station</strong>: <?php echo $airing_station; ?></li>
 				<li class="airing"><strong>Time</strong>: <?php echo $airdate; ?> <?php echo $airtime ?><span id="whatson-timezone"> (<?php echo $timezone; ?>)</span></li>
 				<li class="episode_title"><strong>Episode Title</strong>: <?php echo $episode_title; ?></li>
@@ -44,7 +51,7 @@ function whatson_displayProgramAirdates($programAirdates, $programTitle, $instan
 		unset($upe);
 	} else {
 ?>
-			<ul id="whatson">
+			<ul class="listing">
 				<li><a href="http://www.kqed.org/tv/programs/index.jsp?pgmid=21385"  target="_blank">Get KQED broadcast listings.</a></li>
 				<li><a href="http://www.pbs.org/tv_schedules/" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','http://www.pbs.org']);" target="_blank">Check your local listings.</a></li>
 				<li><a href="http://video.kqed.org/program/film-school-shorts/"  target="_blank">Watch exclusive short films in their original cut.</a></li>
@@ -54,8 +61,11 @@ function whatson_displayProgramAirdates($programAirdates, $programTitle, $instan
 
 	if (strtolower($options['show_zipcode_field']) == 'true') {
 ?>
-			<form id="zipsearchform" method="get" action="#"><input type="text" value="Enter Zip Code" onfocus="if (this.value == 'Enter Zip Code') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Enter Zip code';}" size="18" maxlength="5" name="zlf" id="zipsearchfield">
-			<input type="submit" value="Lookup Zip" id="zipsearchsubmit"></form>
+			<form id="zipsearchform" method="get" action="#">
+				<h4>Find listings for a different location:</h4>
+				<input type="text" value="Enter Zip Code" onfocus="if (this.value == 'Enter Zip Code') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Enter Zip Code';}" size="18" maxlength="5" name="zlf" id="zipsearchfield">
+				<input type="submit" value="Submit" id="zipsearchsubmit">
+			</form>
 <?php 
 	}
 	echo $after_widget;
